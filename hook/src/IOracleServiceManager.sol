bd90a5927c9bb16719c65295eabef1867366f7cc
Yash Goyal <yashgoyaldev@gmail.com>
2025-03-26 18:46:30 +0530

    fix: Task structure params

--- hook/src/IOracleServiceManager.sol
+++ hook/src/IOracleServiceManager.sol
@@ -4,11 +4,7 @@ pragma solidity 0.8.26;
 interface IOracleServiceManager {
     function createNewTask(
         bytes32 poolId,
-        int24 tickLower,
-        int24 tickUpper,
-        int24 localRangeOffset,
         int24 activeTick,
-        int24 tickSpacing,
-        int256[] memory
+        int24 tickSpacing
     ) external;
 }
